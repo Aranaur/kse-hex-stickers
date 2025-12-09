167c2ca721c3d31fd42ae3c18d522b76c1143280
Aranaur <ihor.miroshnychenko@kneu.ua>
2025-12-09 10:51:27 +0200

    modify math840 colors

--- R/math840-25.R
+++ R/math840-25.R
@@ -11,9 +11,9 @@ if (!dir.exists(folder_name)) {
   dir.create(folder_name)
 }
 
-# Start creating the sticker
 
 font_add_google("Roboto Mono", "robotomono")
+font_add_google("Patrick Hand", "patrick") 
 showtext_auto()
 
 electricity_df <- read_csv("https://raw.githubusercontent.com/Aranaur/aranaur.rbind.io/main/datasets/energy_ua/energy_ua_2014_2021.csv") |> 
@@ -22,7 +22,6 @@ electricity_df <- read_csv("https://raw.githubusercontent.com/Aranaur/aranaur.rb
     names_to = "source",
     values_to = "consumption_mwh"
   ) |> 
-  # quarterly consumption
   mutate(
     month = month(time),
     year = year(time),
@@ -34,37 +33,72 @@ electricity_df <- read_csv("https://raw.githubusercontent.com/Aranaur/aranaur.rb
 plot_data <- electricity_df |> 
   filter(source == "TEC")
 
-plot <- acf(plot_data$consumption_mwh, plot = FALSE, lag.max = 15) |> 
-  with(data.frame(lag = lag, acf = acf)) |> 
-  ggplot(aes(x = lag, y = acf)) +
-  geom_segment(aes(xend = lag, yend = 0), color = "#002B36", size = 1) +
-  geom_hline(yintercept = 0, color = "#002B36", size = 1) +
+gradient_line_df <- data.frame(
+  x = seq(min(acf_df$lag) - 0.5, max(acf_df$lag) + 0.5, length.out = 5000),
+  y = 0
+)
+
+horst_palette <- c("#2E5090", "#009B9F", "#5FBC63", "#C4D600")
+
+line_size <- 1.4 
+offset_val <- 0.04
+
+plot <- ggplot() +
+  geom_line(
+    data = gradient_line_df, 
+    aes(x = x, y = y, color = x), 
+    size = line_size, 
+    alpha = 0.8,
+    lineend = "round"
+  ) +
+  
+  geom_segment(
+    data = acf_df,
+    aes(
+      x = lag, xend = lag, 
+      y = sign(acf) * offset_val, 
+      yend = acf, 
+      color = lag
+    ), 
+    size = 1.2, 
+    alpha = 0.8,
+    lineend = "round"
+  ) +
+  
+  geom_point(
+    data = acf_df,
+    aes(x = lag, y = acf, color = lag), 
+    size = 2.8
+  ) +
+  
+  scale_color_gradientn(colors = horst_palette) +
+  theme_void() +
   theme_transparent() +
-  theme(
-    panel.grid = element_blank(),
-    axis.title = element_blank(),
-    axis.text = element_blank(),
-    axis.ticks = element_blank()
-  )
+  theme(legend.position = "none")
 
 sticker_object <- sticker(
   subplot = plot,
-  s_x = 1.0, s_y = 0.8,
-  s_width = 1.5, s_height = 1.4,
+  s_x = 1.0, s_y = 0.85,
+  s_width = 1.6, s_height = 1.2,
+
   package = "MATH840",
   p_family = "robotomono",
   p_fontface = "bold",
   p_size = 62,
-  p_color = "#002B36",
-  p_y = 1.5,
-  h_fill = "#E8EEF2",
-  h_color = "#586E75",
-  url = "KSE@CSD | Time Series | 2025",
+  p_color = "#2E5090",
+  p_y = 1.55,
+
+  h_fill = "#FAFAFA",
+  h_color = "#5FBC63",
+  h_size = 1.5,
+
+  url = "CSD@KSE | Time Series | 2025",
   u_family = "robotomono",
   u_size = 14,
   u_color = "#586E75",
   u_x = 0.99,
-  u_y = 0.07,
+  u_y = 0.08,
+  
   filename = here(folder_name, file_name),
   dpi = 1000
 )
